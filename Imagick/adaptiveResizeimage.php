ff33d778dde9b407163de93e9fe1c7fd27a1a302
Sarthak Prajapati <sarthakprajapati@live.in>
2018-08-12 01:34:50 +0530

    adaptiveResizeImage

--- Imagick/adaptiveResizeimage.php
+++ Imagick/adaptiveResizeimage.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-type: image/png');
 $image = new Imagick('img/geeksforgeeks.png');
-$image->adaptiveResizeImage(2048,1536);
+$image->adaptiveResizeImage(1024,768);
 echo $image;
 ?>
 
