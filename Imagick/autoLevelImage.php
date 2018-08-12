ca9c36c1095a6f754fa440f57cef8da5144b38e4
Sarthak Prajapati <sarthakprajapati@live.in>
2018-08-13 01:09:03 +0530

    autolevel -> borderImage
    autoLevelImage
    averageImages
    blackThresholdImage
    blueShiftImage
    blurImage
    borderImage

---
+++ Imagick/autoLevelImage.php
@@ -0,0 +1,11 @@
+<?php 
+require_once('vendor/autoload.php'); 
+header('Content-type: image/png');
+$image = new Imagick('img/geeksforgeeks.png');
+$image->autoLevelImage(Imagick::CHANNEL_DEFAULT);
+echo $image;
+?>
+
+
+
+
